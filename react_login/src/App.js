e4d34006586d55a0a9bc9f4ffe01589cb1b1403b
pop_apd <98011000+popapideth@users.noreply.github.com>
2024-10-27 20:47:29 +0700

    add frontend

---
+++ react_login/src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+
+  useEffect(() => {
+
+  },[])
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
